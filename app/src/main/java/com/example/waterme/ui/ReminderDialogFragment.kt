e9017e43f7f70b20c98a39184647b141bb1506cf
Maxmarra <5919669@gmail.com>
2022-07-08 13:17:44 +0300

    drilling_01

--- app/src/main/java/com/example/waterme/ui/ReminderDialogFragment.kt
+++ app/src/main/java/com/example/waterme/ui/ReminderDialogFragment.kt
@@ -1,18 +1,3 @@
-/*
- * Copyright (C) 2021 The Android Open Source Project.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.waterme.ui
 
 import android.app.AlertDialog
@@ -39,7 +24,7 @@ class ReminderDialogFragment(private val plantName: String) : DialogFragment() {
                     when (position) {
                         0 ->
                             viewModel
-                                .scheduleReminder(5, TimeUnit.SECONDS, plantName)
+                                .scheduleReminder(15, TimeUnit.SECONDS, plantName)
                         1 ->
                             viewModel
                                 .scheduleReminder(1, TimeUnit.DAYS, plantName)
